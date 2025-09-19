626e224a0180f9d990085d611bc16f3a506367c1
Maxime Bellet <maxime.bellet@wit.fr>
2025-09-19 15:12:10 +0200

    chore: setup www app

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,24 +2,32 @@ import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import ts from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 
 export const config = ts.config(
 	js.configs.recommended,
 	...ts.configs.recommended,
-	...svelte.configs['flat/recommended'],
+	...svelte.configs.recommended,
+	prettier,
+	...svelte.configs.prettier,
 	{
 		languageOptions: {
 			globals: {
 				...globals.browser,
 				...globals.node
+			},
+			rules: {
+				'no-undef': 'off'
 			}
 		}
 	},
 	{
-		files: ['**/*.svelte'],
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		ignores: ['.svelte-kit/*'],
 		languageOptions: {
 			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
 				parser: ts.parser
 			}
 		}
