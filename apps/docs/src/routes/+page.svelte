05fb6bc5358aebf1e90bf3de38b79b4a6d700c43
Maxime Bellet <maxime.bellet@wit.fr>
2025-09-19 11:23:55 +0200

    chore: run prettier format

--- apps/docs/src/routes/+page.svelte
+++ apps/docs/src/routes/+page.svelte
@@ -4,4 +4,6 @@
 
 <h1>Docs</h1>
 <MyCounterButton />
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
